f39be393af94b1011df48f944cff553b2d4cc018
Steffi Wong <111824867+mimilame02@users.noreply.github.com>
2023-05-29 10:43:12 +0800

    .

--- style/js/sendmail.js
+++ style/js/sendmail.js
@@ -1,10 +1,46 @@
 function submitForm() {
     event.preventDefault(); // Prevents the default form submission behavior
 
-    sendMail();
+    if (validateForm()) {
+        sendMail();
+    }
 }
 
-function sendMail(){
+function validateForm() {
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const messageInput = document.getElementById("message");
+
+    if (nameInput.value.trim() === "") {
+        alert("Please enter your name.");
+        nameInput.focus();
+        return false;
+    }
+
+    if (emailInput.value.trim() === "") {
+        alert("Please enter your email.");
+        emailInput.focus();
+        return false;
+    }
+
+    // Email format validation using regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(emailInput.value)) {
+        alert("Please enter a valid email address.");
+        emailInput.focus();
+        return false;
+    }
+
+    if (messageInput.value.trim() === "") {
+        alert("Please enter your message.");
+        messageInput.focus();
+        return false;
+    }
+
+    return true;
+}
+
+function sendMail() {
     var params = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
@@ -15,13 +51,13 @@ function sendMail(){
     const templateID = "template_ffkujd4";
 
     emailjs
-    .send(serviceID, templateID, params)
+        .send(serviceID, templateID, params)
         .then((res) => {
             document.getElementById("name").value = "";
             document.getElementById("email").value = "";
             document.getElementById("message").value = "";
             console.log(res);
-            alert("Your message sent successfully.");
+            alert("Your message was sent successfully.");
         })
         .catch((err) => console.log(err));
 }
